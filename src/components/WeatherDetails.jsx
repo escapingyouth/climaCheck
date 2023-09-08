990de307a9046401026dea8087e2be23913f3301
Michael Appiah <mke.appiah@gmail.com>
2023-09-08 15:49:03 +0000

    Add weather details component

---
+++ src/components/WeatherDetails.jsx
@@ -0,0 +1,39 @@
+import PropTypes from 'prop-types';
+
+WeatherDetails.propTypes = {
+	weather: PropTypes.object
+};
+
+function WeatherDetails({
+	weather: { cloudiness, humidity, speed, pressure }
+}) {
+	return (
+		<div className='px-14'>
+			<h2 className='text-secondary font-normal mb-4'>Weather Details</h2>
+			<ul
+				className='[&>li]:flex [&>li]:justify-between [&>li]:items-center 
+									  [&>li]:mb-4 border-b-[1px] pb-2 border-primary [&>li>h3]:text-secondary
+									  [&>li>h3]:font-semibold'
+			>
+				<li>
+					<span>Cloudy</span>
+					<h3>{cloudiness}%</h3>
+				</li>
+				<li>
+					<span>Humidity</span>
+					<h3>{humidity}%</h3>
+				</li>
+				<li>
+					<span>Wind</span>
+					<h3>{speed} m/s</h3>
+				</li>
+				<li>
+					<span>Pressure</span>
+					<h3>{pressure} hPa</h3>
+				</li>
+			</ul>
+		</div>
+	);
+}
+
+export default WeatherDetails;
